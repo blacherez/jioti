3a84c7184c87137415253414fd672cd218aff35c
Benoit Lacherez <blacherez@gmail.com>
2018-12-28 17:44:44 +0100

    Ajout de templates pour la page d'accueil

--- licornes/views.py
+++ licornes/views.py
@@ -2,7 +2,16 @@
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
 
+from .models import Licorne
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the unicorns index.")
+    #return HttpResponse("Hello, world. You're at the unicorns index.")
+
+    #latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('licornes/index.html')
+    context = {
+        'latest_question_list': 0,
+    }
+    return HttpResponse(template.render(context, request))
