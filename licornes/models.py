4496bd341f1de9272bf89d19d5ac44553d82bf97
Benoit Lacherez <blacherez@gmail.com>
2018-12-28 17:19:08 +0100

    Amélioration des modèles

--- licornes/models.py
+++ licornes/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Licorne(models.Model):
     nom = models.CharField(max_length=200)
-    creation_date = models.DateTimeField('Date de création')
+    creation_date = models.DateField('Date de création')
     photo = models.CharField(max_length=200)
     identifiant = models.CharField(max_length=50)
 
@@ -13,7 +13,9 @@ class Licorne(models.Model):
 
 class Etape(models.Model):
     licorne = models.ForeignKey(Licorne, on_delete=models.CASCADE)
-    etape_date = models.DateTimeField()
+    etape_date = models.DateField()
+    localisation = models.CharField(max_length=100)
+    current = models.BooleanField()
 
     def __str__(self):
         return ("Etape de %s (%s)" % (self.licorne.nom, self.etape_date))
