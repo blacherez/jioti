6a4b8acec8dd4a50a0b47bb21be584763c9a92f1
Benoit Lacherez <blacherez@gmail.com>
2018-12-29 00:03:52 +0100

    Suppression de la clé secrète du fichier de base

--- jiji/conf/base.py
+++ jiji/conf/base.py
@@ -19,11 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'r4*!b^4cshi6f@_z5hc2mu_)8-ed66renhs_7c*n3u(+$=-5!1'
-
-
-
 # Application definition
 
 INSTALLED_APPS = [
