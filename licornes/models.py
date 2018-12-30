8be2c049b3c624bdd316eebffc42667f36fb0bec
Benoit Lacherez <blacherez@gmail.com>
2018-12-30 11:44:18 +0100

    Modification du modèle Etape (autoriser des champs nuls)

--- licornes/models.py
+++ licornes/models.py
@@ -22,14 +22,14 @@ class Licorne(models.Model):
 
 class Etape(models.Model):
     licorne = models.ForeignKey(Licorne, on_delete=models.CASCADE)
-    etape_date = models.DateField()
-    localisation = models.CharField(max_length=100, default=None)
+    etape_date = models.DateField(default=date.today)
+    localisation = models.CharField(max_length=100, null=True)
     current = models.BooleanField(default=False)
     auteur = models.ForeignKey(
       settings.AUTH_USER_MODEL,
       on_delete=models.CASCADE
     )
-    media = models.CharField(max_length=200, default=None)
+    media = models.CharField(max_length=200, null=True)
 
     def __str__(self):
         return ("Etape de %s (%s)" % (self.licorne.nom, self.etape_date))
