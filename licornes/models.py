7040b30ab842060ce3466d344afd198b062782a3
Benoit Lacherez <blacherez@gmail.com>
2018-12-30 16:59:59 +0100

    Modification des modèles pour permettre des champs vides

--- licornes/models.py
+++ licornes/models.py
@@ -10,7 +10,7 @@ from django.conf import settings
 class Licorne(models.Model):
     nom = models.CharField(max_length=50)
     creation_date = models.DateField('Date de création', default=date.today)
-    photo = models.CharField(max_length=200, null=True)
+    photo = models.CharField(max_length=200, null=True, blank=True)
     identifiant = models.CharField(max_length=50)
     createur = models.ForeignKey(
       settings.AUTH_USER_MODEL,
@@ -23,13 +23,13 @@ class Licorne(models.Model):
 class Etape(models.Model):
     licorne = models.ForeignKey(Licorne, on_delete=models.CASCADE)
     etape_date = models.DateField(default=date.today)
-    localisation = models.CharField(max_length=200, null=True)
+    localisation = models.CharField(max_length=200, null=True, blank=True)
     current = models.BooleanField(default=False)
     auteur = models.ForeignKey(
       settings.AUTH_USER_MODEL,
       on_delete=models.CASCADE
     )
-    media = models.CharField(max_length=200, null=True)
+    media = models.CharField(max_length=200, null=True, blank=True)
 
     def __str__(self):
         return ("Etape de %s (%s)" % (self.licorne.nom, self.etape_date))
