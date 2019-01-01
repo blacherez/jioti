c3b8955f65ac471a9eeb5c0924c565f5d61b49cb
Benoit Lacherez <blacherez@gmail.com>
2019-01-01 17:31:28 +0100

    Correction issue 1 : Impossible de créer deux licornes avec le même identifiant

--- licornes/tests/test_models.py
+++ licornes/tests/test_models.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 
+from django.db import IntegrityError
+
 # Create your tests here.
 from licornes.models import Licorne
 from licornes.models import User
@@ -13,43 +15,53 @@ class LicorneModelTest(TestCase):
         # Set up non-modified objects used by all test methods
         User.objects.create(username="prof")
         u = User.objects.get(username="prof")
-        Licorne.objects.create(nom='Chamallow', identifiant='77898787676867', createur=u)
+        cls.u = u
+        cls.identifiant_utilise = '77898787676867'
+        Licorne.objects.create(nom='Chamallow', identifiant=cls.identifiant_utilise, createur=u)
 
     def test_label_nom(self):
-        licorne = Licorne.objects.get(identifiant='77898787676867')
+        licorne = Licorne.objects.get(identifiant=self.identifiant_utilise)
         field_label = licorne._meta.get_field('nom').verbose_name
         self.assertEquals(field_label, 'nom')
 
     def test_label_creation_date(self):
-        licorne=Licorne.objects.get(identifiant='77898787676867')
+        licorne=Licorne.objects.get(identifiant=self.identifiant_utilise)
         field_label = licorne._meta.get_field('creation_date').verbose_name
         self.assertEquals(field_label, 'Date de création')
 
     def test_max_length_nom(self):
-        licorne = Licorne.objects.get(identifiant='77898787676867')
+        licorne = Licorne.objects.get(identifiant=self.identifiant_utilise)
         max_length = licorne._meta.get_field('nom').max_length
         self.assertEquals(max_length, 50)
 
     def test_max_length_photo(self):
-        licorne = Licorne.objects.get(identifiant='77898787676867')
+        licorne = Licorne.objects.get(identifiant=self.identifiant_utilise)
         max_length = licorne._meta.get_field('photo').max_length
         self.assertEquals(max_length, 200)
 
     def test_max_length_identifiant(self):
-        licorne = Licorne.objects.get(identifiant='77898787676867')
+        licorne = Licorne.objects.get(identifiant=self.identifiant_utilise)
         max_length = licorne._meta.get_field('identifiant').max_length
         self.assertEquals(max_length, 50)
 
     def test_object_name_is_nom(self):
-        licorne = Licorne.objects.get(identifiant='77898787676867')
+        licorne = Licorne.objects.get(identifiant=self.identifiant_utilise)
         expected_object_name = f'{licorne.nom}'
         self.assertEquals(expected_object_name, str(licorne))
 
     def test_creation_date_est_date_du_jour_par_defaut(self):
-        licorne = Licorne.objects.get(identifiant='77898787676867')
+        licorne = Licorne.objects.get(identifiant=self.identifiant_utilise)
         expected_date = date.today()
         self.assertEquals(expected_date, licorne.creation_date)
         # def test_get_absolute_url(self):
+
+    def test_identifiant_est_unique(self):
+        creation_echoue = False
+        try:
+            Licorne.objects.create(nom='Chabooky', identifiant=self.identifiant_utilise, createur=self.u)
+        except IntegrityError:
+            creation_echoue = True
+        self.assertTrue(creation_echoue)
 
     #     author = Author.objects.get(id=1)
     #     # This will also fail if the urlconf is not defined.
