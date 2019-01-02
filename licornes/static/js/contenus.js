fcc4672297fe5d293a5a4183e167af90320d4871
Benoit Lacherez <blacherez@gmail.com>
2019-01-02 17:43:39 +0100

    Fin de l'implémentation de l'affichage des médias d'étapes sous forme d'embeds

--- licornes/static/js/contenus.js
+++ licornes/static/js/contenus.js
@@ -3,7 +3,7 @@ function remplacerMap(url) {
     //$("#map").html(url);
     //$( "#map" ).html('<iframe width="560" height="315" src="https://www.youtube.com/embed/ixv_6mJuaSY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
     $.get( url, function( data ) {
-         $( "#map" ).html('<p>' + data["media"] + '</p>' );
+         $( "#map" ).html( data["media"] );
          //alert( data["media"] );
        });
 
