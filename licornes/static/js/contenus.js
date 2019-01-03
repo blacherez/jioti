f32bf7ecf4c1504df9d61dc108c14d83d50e3f25
Benoit Lacherez <blacherez@gmail.com>
2019-01-04 00:56:51 +0100

    Correction bug

--- licornes/static/js/contenus.js
+++ licornes/static/js/contenus.js
@@ -3,7 +3,6 @@ function remplacerMap(url) {
     //$("#map").html(url);
     //$( "#map" ).html('<iframe width="560" height="315" src="https://www.youtube.com/embed/ixv_6mJuaSY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
     $.get( url, function( data ) {
-      $("#etapes")
       $( "#map" ).html( "<p>" + data["localisation"] + " (" + data["date"] + ")</p>" + data["media"] );
       //alert( data["media"] );
        });
