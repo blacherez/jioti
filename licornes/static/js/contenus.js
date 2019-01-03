d23b0f111e9d06537d9f2b96e07a260393fc8869
Benoit Lacherez <blacherez@gmail.com>
2019-01-04 00:42:08 +0100

    Le titre "Trajet" est remplacé par les numéros d'étapes quand on clique sur le bouton de l'étape.

--- licornes/static/js/contenus.js
+++ licornes/static/js/contenus.js
@@ -3,8 +3,9 @@ function remplacerMap(url) {
     //$("#map").html(url);
     //$( "#map" ).html('<iframe width="560" height="315" src="https://www.youtube.com/embed/ixv_6mJuaSY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
     $.get( url, function( data ) {
-         $( "#map" ).html( "<p>" + data["localisation"] + " (" + data["date"] + ")</p>" + data["media"] );
-         //alert( data["media"] );
+      $("#etapes")
+      $( "#map" ).html( "<p>" + data["localisation"] + " (" + data["date"] + ")</p>" + data["media"] );
+      //alert( data["media"] );
        });
 
 }
